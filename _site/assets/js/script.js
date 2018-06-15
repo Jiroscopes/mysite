6394bfcde7706621716a743801dbf4f4f21916e2
Jiroscopes <steven.popick@gmail.com>
2018-06-14 21:10:58 -0400

    Completed My Work section
    the script is targeting the ul by artibtrary data attribute 'images' which is attached to the thumbnails.

---
+++ _site/assets/js/script.js
@@ -0,0 +1,41 @@
+$(document).ready(function(){
+    var triggers = $('ul.triggers li');
+    var images = $('ul.images');
+    var lastElem = triggers.length-1;
+    var imgWidth = images.width();
+    var target;
+
+    triggers.first().addClass('selected');
+    
+    function sliderResponse(target){
+        console.log(target);
+        $('.images').stop(true,false).animate({'left':'-'+ imgWidth*target +'px'},300);
+        triggers.removeClass('selected').eq(target).addClass('selected');
+    }
+
+    triggers.click(function(e){
+        if (!$(this).hasClass('selected')){
+            target = $(this).index();
+            sliderResponse(target);
+        };
+    });
+
+    $('.gallery_img').on('click', function(){
+        $('.work-modal').addClass('active_modal');
+        $('.custom_modal').addClass('active_custom_modal');
+    });
+    $('.under_modal').on('click', function(){
+        $('.work-modal').removeClass('active_modal');
+        $('.custom_modal').removeClass('active_custom_modal');
+        $('.images').addClass('hidden').delay(2000);
+    });
+
+    // Image Gallery Stuff
+    $('.gallery_img').on('click', function(){
+        var imageSetId = $(this).data('images');
+        $('#' + imageSetId).removeClass('hidden');
+        console.log(imageSetId)
+    });
+
+    // console.log(imgWidth)
+})
